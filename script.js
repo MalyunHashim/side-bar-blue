8df9119938c1306e448f6753b92925489e58684a
malyumohamed <malyunmohaz942@gmail.com>
2023-09-29 19:09:00 +0300

    made some minor improvements on the hover effect

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("hi");
+$(".menu > ul > li").click(function (e){
+    //remove active from alredy active
+    $(this).siblings().removeClass("active");
+    //add active to clicked 
+  $(this).toggleClass("active");
+  //if has sub menu open it
+  $(this).find("ul").slideToggle();
+   //close other sub menu if any open
+   $(this).siblings.find("ul").slideup();
+   //remove active class of sub menu items
+   $(this).siblings().find("li").removeClass("active");
+});
